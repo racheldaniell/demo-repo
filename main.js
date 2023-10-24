bfba74e09f6fc77c554c359043adf8c22e9f7dbe
racheldaniell <44450960+racheldaniell@users.noreply.github.com>
2023-10-24 15:59:56 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,51 @@
+/* first JS file! */
+/**  DOM MANIPULATION  **/
+
+
+// 1 - A SIMPLE MESSAGE BUTTON 
+
+// First we need to find/get/grab the related piece in HTML
+// declare a const variable to "grab" the html element 
+// name it something intuitive in camelCase
+// use the methods beginning in document.getElementBy... 
+
+
+
+// write a function - something that will happen onClick 
+// pay careful attention to syntax/punctuation here 
+
+
+  // show a message when user clicks 
+
+
+
+
+// 2 - CHANGE THE HTML ON SCREEN USING JS!
+
+// declare and intialize a const variable to grab your HTML button
+// declare and initialize a const variable to grab the HTML class/element/id you want to impact
+// write a function to change text color
+// THEN update HTML to have name of your function on click
+// could create another button to undo this or create a radio button selection
+
+// const myColorText = document.getElementById("color-text");
+
+
+
+// 3 - MORE COMPLEXITY - 3 PIECES WORKING TOGETHER
+
+// Now add 3 things to work together 
+// add 3 CONST variables to hold interactive contennt
+
+
+
+
+
+   // update the name using the value of the input element
+
+
+
+   // use the user's name in a window alert using templating
+   // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+
